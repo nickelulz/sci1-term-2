28805bfac20c7c79d1511fd5b1e1d14cba3ba2d1
Boneclad84 <matsunagai84@gmail.com>
2025-02-11 14:49:42 -0800

    First reverse_engineer.py (Calix Mark 1)

--- src/reverse_engineer.py
+++ src/reverse_engineer.py
@@ -1,6 +1,25 @@
-def reverse_engineer_model(output_data_sequence, convergence_benchmark):
-  """
-  Interprets the output data sequence and the convergence benchmark
-  to develop a new CoinFlipEngine model that imitates the original.
-  """
-  raise NotImplementedError('reverse_engineer_model(output_data_sequence, convergence_benchmark)')
+def Calix(output_data_sequence, markov, n, delta=0.01):
+  #n = number of outputs
+  estimated_model = np.random.rand(n, n) * 100
+  observations = np.zeros(shape=(n,n))
+  cur = 0 #start storing values in the first coin by default
+  epoch=0
+  for obs in output_data_sequence:
+    i = markov[cur].index(obs)
+    observations[cur][i] += 1
+    cur = i
+    row_sums = observations.sum(axis=1, keepdims=True)
+    row_sums[row_sums == 0] = 1
+    gradients = (observations / row_sums)*100 - estimated_model
+    estimated_model += delta * gradients
+    if epoch % 2500 == 0:
+      print(f"Cur Est: {estimated_model}")
+      print(f"Cur Obs: {observations}")
+      print(f"Obs Markov: {(observations / row_sums)*100}")
+    epoch+=1
+  return CoinFlipEngine(
+    probabilities = estimated_model,
+    markov = np.array(markov),
+    number_of_coins = n,
+    number_of_outputs = n,
+    initial_coin_index = 0)
