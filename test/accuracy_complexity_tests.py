8603ab496367d3b8116f8f15aacc5e4dad58a898
Boneclad84 <matsunagai84@gmail.com>
2025-02-12 15:28:39 -0800

    Reverse engineered coin Accuracy test function completed accuracy_complexity_tests.py

--- test/accuracy_complexity_tests.py
+++ test/accuracy_complexity_tests.py
@@ -10,3 +10,11 @@ from coin_examples import *
 
 class AccuracyComplexityTests(TestCase):
     def same_coin_accuracy_test(self):
+
+def testAccuracy(revCoin, dataProbs, flips=int(1e4)):
+  n = 10 #arbitraty number of times we test it
+  accuracies = []
+  for i in range(n):
+    res = perform_coin_flips(revCoin, flips)
+    accuracies.append(max(res["empirical_probabilities"] - dataProbs))
+  return np.mean(accuracies)
