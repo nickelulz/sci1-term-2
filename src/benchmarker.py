fbe85229a9443dcf1f69b1ea6e739cd8fb6eee7c
mufaro <nickelulz@proton.me>
2025-02-15 22:24:03 -0800

    idfk at this point

--- src/benchmarker.py
+++ src/benchmarker.py
@@ -1,41 +1,36 @@
 import numpy as np
 
-def calculate_accuracy(distribution_mse, complexity_pe):
+def calculate_distribution_error(distribution_a, distribution_b):
   """
-  Calculates the accuracy index between two models
+  Calculates the error between the two prrobability distributions
   """
-  return distribution_mse * (complexity_pe * 100)
+  return np.sum(np.abs(distribution_a - distribution_b))
 
-"""
-The error tolerance for how far each of the empirical
-distributions can be from the expected distribution.
-"""
-BENCHMARK_ERROR_TOLERANCE = 1e-2
+def calculate_complexity_error(complexity_a, complexity_b):
+    """
+    Calculates the percent error (or absolute error, if one or both is zero) 
+    between two models
+    """
+    complexity_pe = -1
 
-def benchmark_distribution_error(distribution_a, distribution_b):
-  """
-  Calculates the Mean-Squared Error between the two distributions
-  CONSTRAINT: must be the same size
-  """
-  return (np.sum(np.abs(distribution_a - distribution_b) ** 2) /
-          len(distribution_a))
+    if complexity_a == 0 or complexity_b == 0:
+        complexity_pe = max(complexity_a, complexity_b)
+    else:
+        complexity_pe = (np.abs(complexity_a - complexity_b) / complexity_a)
+
+    return complexity_pe
 
 def calculate_model_error(input_model, output_model) -> float:
     """
-    Compares two CoinFlipEngine models by calculating the accuracy
+    Compares two CoinFlipEngine models by calculating the error 
     of one model against the other.
     """
     if input_model.size != output_model.size:
         return np.inf
 
-    distribution_error = benchmark_distribution_error(input_model.theoretical_distribution, 
-                                                      output_model.theoretical_distribution)
-    
-    complexity_percent_error = 0
+    distribution_mse = calculate_distribution_error(input_model.probabilities / 100, 
+                                                    output_model.probabilities / 100)
+    complexity_pe = calculate_complexity_error(input_model.complexity,
+                                               output_model.complexity)
 
-    if input_model.complexity == 0 or output_model.complexity == 0:
-        complexity_percent_error = max(output_model.complexity, input_model.complexity)
-    else:
-        complexity_percent_error = (np.abs(input_model.complexity - output_model.complexity) / input_model.complexity)
-
-    return calculate_accuracy(distribution_error, complexity_percent_error)
+    return distribution_mse + complexity_pe
--- src/generator.py
+++ src/generator.py
@@ -9,7 +9,7 @@ def generate_random_model(max_size=2, min_size=1):
     Generates a random CoinFlipEngine model with pseudorandom weights.
     """
 
-    size = random.randint(max_size, max_size)
+    size = random.randint(min_size, max_size)
     probabilities_matrix = np.array([generate_random_distribution(size) for n in range(size)]).reshape(size, size)
     markov_matrix = np.array([n for n in range(size)] * size).reshape(size, size)
 
